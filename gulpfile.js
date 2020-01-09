fd4ea950f29f62c9e2712faf4b1d8d1643850549
James Lane <lanejames35@gmail.com>
2020-01-09 10:06:27 -0500

    convert gulp syntax to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,34 @@
 'use strict';
 // Generated on 2020-01-09 using generator-leaflet 0.0.17
 
-var gulp = require('gulp');
-var open = require('open');
-var wiredep = require('wiredep').stream;
+const { series, src, dest, watch } = require('gulp');
+const open = require('open');
 
 // Load plugins
-var $ = require('gulp-load-plugins')();
+const $ = require('gulp-load-plugins')();
 
 // Styles
-gulp.task('styles', function () {
-    return gulp.src(['app/styles/main.css'])
+function styles() {
+    return src(['app/styles/main.css'])
         .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('app/styles'))
+        .pipe(dest('app/styles'))
         .pipe($.size());
-});
+};
 
 // Scripts
-gulp.task('scripts', function () {
-    return gulp.src(['app/scripts/**/*.js'])
+function scripts() {
+    return src(['app/scripts/**/*.js'])
         .pipe($.jshint('.jshintrc'))
         .pipe($.jshint.reporter('default'))
         .pipe($.size());
-});
+};
 
 // HTML
-gulp.task('html', ['styles', 'scripts'], function () {
-    var jsFilter = $.filter('**/*.js');
-    var cssFilter = $.filter('**/*.css');
+function html() {
+    const jsFilter = $.filter('**/*.js');
+    const cssFilter = $.filter('**/*.css');
 
-    return gulp.src('app/*.html')
+    return src('app/*.html')
         .pipe($.useref.assets())
         .pipe(jsFilter)
         .pipe($.uglify())
@@ -39,85 +38,65 @@ gulp.task('html', ['styles', 'scripts'], function () {
         .pipe(cssFilter.restore())
         .pipe($.useref.restore())
         .pipe($.useref())
-        .pipe(gulp.dest('dist'))
+        .pipe(dest('dist'))
         .pipe($.size());
-});
+};
 
 // Images
-gulp.task('images', function () {
-    return gulp.src([
+function images() {
+    return src([
     		'app/images/**/*',
     		'app/lib/images/*'])
-        .pipe(gulp.dest('dist/images'))
+        .pipe(dest('dist/images'))
         .pipe($.size());
-});
+};
 
 // Clean
-gulp.task('clean', function () {
-    return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], { read: false }).pipe($.clean());
-});
-
-// Build
-gulp.task('build', ['html', 'images']);
-
-// Default task
-gulp.task('default', ['clean'], function () {
-    gulp.start('build');
-});
+function clean() {
+    return src(['dist/styles', 'dist/scripts', 'dist/images'], { read: false }).pipe($.clean());
+};
 
 // Connect
-gulp.task('connect', function(){
+function connect(){
     $.connect.server({
         root: 'app',
         port: 9000,
         livereload: true
     });
-});
+};
 
 // Open
-gulp.task('serve', ['connect'], function() {
+function serve() {
   open("http://localhost:9000");
-});
+};
 
-// Inject Bower components
-gulp.task('wiredep', function () {
-    gulp.src('app/styles/*.css')
-        .pipe(wiredep({
-            directory: 'app/bower_components',
-            ignorePath: 'app/bower_components/'
-        }))
-        .pipe(gulp.dest('app/styles'));
-
-    gulp.src('app/*.html')
-        .pipe(wiredep({
-            directory: 'app/bower_components',
-            ignorePath: 'app/'
-        }))
-        .pipe(gulp.dest('app'));
-});
-
-// Watch
-gulp.task('watch', ['connect', 'serve'], function () {
+function watchTask() {
     // Watch for changes in `app` folder
-    gulp.watch([
+    watch([
         'app/*.html',
         'app/styles/**/*.css',
         'app/scripts/**/*.js',
         'app/images/**/*'
     ], function (event) {
-        return gulp.src(event.path)
+        return src(event.path)
             .pipe($.connect.reload());
     });
 
     // Watch .css files
-    gulp.watch('app/styles/**/*.css', ['styles']);
+    watch('app/styles/**/*.css', styles);
 
     // Watch .js files
-    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    watch('app/scripts/**/*.js', scripts);
 
     // Watch image files
-    gulp.watch('app/images/**/*', ['images']);
+    watch('app/images/**/*', images);
+};
 
-    // Watch bower files
-    gulp.watch('bower.json', ['wiredep']);
-});
+// Build
+exports.build = series(html, images);
+
+// Default task
+exports.default = series(html, images, clean);
+
+// Watch
+exports.watch = series(connect, serve, watchTask);
