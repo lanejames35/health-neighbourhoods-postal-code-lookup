d352a601d4967bd5dfa0b9f9ab723bffcdbef64f
James Lane <lanejames35@gmail.com>
2020-01-15 10:58:16 -0500

    refine the mouse event for the hn layer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,7 @@ var map = L.map('map').setView([43.898206, -78.940707], 9);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	minZoom: 0,
 	maxZoom: 18,
-	attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+	attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a> | Postal Code data © Canada Post Corporation'
 }).addTo(map);
 // Create the custom dialog to show the neighbourhood name
 var info = L.control();
@@ -18,72 +18,64 @@ info.onAdd = function() {
 // method that we will use to update the control based on feature properties passed
 info.update = function(props) {
 	this._div.innerHTML = '<h4>Neighbourhood name</h4>' +  (props ?
-		'<b>' + props.COMMON_NAME + '</b>' : 'Hover over a neighbourhood');
+		'<b>' + props.COMMON_NAME + '</b>' : 'Hover a neighbourhood border');
 };
 // add the information controller
 info.setPosition('bottomright').addTo(map);
 
-// Highlight the area when hovered
-function highlightFeature(e) {
-	var layer = e.target;
-	layer.setStyle({
-		weight: 5,
-		color: '#666',
-		dashArray: '',
-		fillOpacity: 0.7
-	});
-	if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-		layer.bringToFront();
-	}
-	console.log("the mouse is in");
-	info.update(layer.feature.properties);
-}
-
-var hnLayer, fsaLayer;
-// Reset the highlight when the mouse is moved out
-function resetHighlight(e) {
-	hnLayer.resetStyle(e.target);
-	info.update();
-	console.log("the mouse is out");
-}
-// Zoom in on the neighbourhood when clicked
-function zoomToFeature(e) {
-	map.fitBounds(e.target.getBounds());
-}
-function onEachFeature(feature, layer){
-	layer.on({
-		mouseover: highlightFeature,
-		mouseout: resetHighlight,
-		click: zoomToFeature
-	});
-}
 // Create the FSA layer
-fsaLayer = L.geoJSON(fsa, {
+const fsaLayer = L.geoJSON(fsa, {
 	style: function() {
-		const colours = ['#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e','#e6ab02'];
-		const idx = Math.floor(Math.random() * 6);
-		return { color: colours[idx], fill: true };
+		return { color: '#d95f02', fill: true };
 	},
 	onEachFeature: function(feature, layer){
 		layer.bindTooltip(layer.feature.properties.FSA, { sticky: true, interactive: true });
 		layer.on({
 			mouseover: function(e){
 				e.target.openTooltip();
-				console.log("Mouseover");
 			},
 			mouseout: function(e){
 				e.target.closeTooltip();
-				console.log("Mouse is out");
 			},
 			click: function(e){
-				e.target.toggleTooltip();
 				e.target.bringToFront();
 			}
 		})
 	}
 }).addTo(map);
 // Create the neighbourhood geoJSON layer
-hnLayer = L.geoJSON(hn, { onEachFeature: onEachFeature }).addTo(map);
+const hnLayer = L.geoJSON(hn, {
+	style: function(){
+		return { color: '#3388ff', fill: false};
+	},
+	onEachFeature: function(feature, layer){
+		layer.on({
+			mouseover: function(e){
+				const currentColour = (e.target.options.color == '#1eb300' ? '#1eb300' : '#666');
+				layer.setStyle({
+					weight: 5,
+					color: currentColour,
+				});
+				info.update(layer.feature.properties);
+			},
+			mouseout: function(e){
+				const currentColour = (e.target.options.color == '#1eb300' ? '#1eb300' : '#3388ff');
+				layer.setStyle({
+					weight: 3,
+					color: currentColour
+				});
+				info.update();
+			},
+			click: function(e){
+				const currentColour = (e.target.options.color == '#1eb300' ? '#666' : '#1eb300');
+				layer.setStyle({
+					color: currentColour
+				});
+				layer.bringToFront();
+			}
+		})
+	}
+}).addTo(map);
 
 /*
  * Layer toggle control
